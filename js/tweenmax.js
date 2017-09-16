627e27a4db397c9771485b6a5be97c9d4eca6528
sexfat <bryant6831@gmail.com>
2017-09-16 16:54:00 +0800

    add tweenmax

---
+++ js/tweenmax.js
@@ -0,0 +1,29 @@
+// work
+
+$(function(){
+
+TweenMax.fromTo( ".section_1 .title" , 1 , {
+    y : -50,
+    opacity : 0,
+}, {
+   y : 0,
+   opacity : 1,
+   repeat : 2,
+   ease : Quad.easeOut
+});
+
+TweenMax.fromTo( ".section_1 .content" , 1 , {
+    x : -50,
+    opacity : 0,
+}, {
+   x : 0,
+   opacity : 1,
+   delay : 0.5,
+   ease : Quad.easeOut
+});
+
+console.log('section_1 ok');
+
+
+})
+
