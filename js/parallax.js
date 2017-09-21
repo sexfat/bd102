25571b0e24d983f4f95b8f69430e1bd568bc9c47
sexfat <bryant6831@gmail.com>
2017-09-21 18:03:46 +0800

    Add tweenmax

--- js/parallax.js
+++ js/parallax.js
@@ -36,12 +36,74 @@ $(function () {
     // var objects = ['.text1', '.text2', '.text3', '.text4', '.text5'];
     TweenMax.staggerFromTo('.com', 1, {
         opacity: 0,
-        y : -10
+        y: -10
     }, {
         opacity: 1,
-        y : 0
+        y: 0
 
     }, 0.5);
     console.log('tween st ok');
+
+    var text = $('#btn_hover'),
+        area = $('#btn_scare'),
+        move = $('#btn_move');
+
+
+    $(function () {
+        ////hover 事件
+        // text.hover(function() {
+        //     //做事情
+        //     TweenMax.staggerFromTo('.comw', 1, {
+        //         opacity: 0,
+        //         y : -10
+        //     }, {
+        //         opacity: 1,
+        //         y : 0
+
+        //     }, 0.5);
+        //     //結束
+        //     })
+
+        text.on('click', function () {
+            //做事情
+            TweenMax.staggerFromTo('.comw', 1, {
+                opacity: 0,
+                y: -10
+            }, {
+                opacity: 1,
+                y: 0
+            }, 0.5);
+        });
+        ////放大
+        area.on('click', function () {
+            //做事情
+            TweenMax.staggerFromTo('.boxarea', 0.5, {
+                opacity: 0,
+                y: -10
+            }, {
+                opacity: '+=0.4',
+                y: 0,
+                scale: 1.5,
+                rotation: 360
+            }, 0.5);
+        });
+
+        //parallax
+        var scene = document.getElementById('parallax_box');
+        var parallax = new Parallax(scene);
+
+        ////
+        move.on('click', function () {
+            TweenMax.fromTo('.animation_box', 1, {
+                width: 0
+            },{
+                 width: '100%',
+                 ease: Power4.easeOut
+            });
+            //用to 也同樣可以做到
+        });
+    });
+
+
 
 });
