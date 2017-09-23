99e50895a5465f4250632f6e273700ad5b34300b
sexfat <bryant6831@gmail.com>
2017-09-23 10:35:06 +0800

    add 不同物件 跑一樣動畫

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -25,21 +25,37 @@ TweenMax.fromTo(".section_1 .content", 1, {
 console.log('section_1 ok');
 
 //四個物件動畫
-// TweenMax.staggerFromTo('.wide_4x .item', .8, {
-//     //做事情
-//     opacity: 0,
-//     y: -50
-// }, {
-//     opacity: 1,
-//     y: 0
-// }, 1)
-var wp = new TimelineMax().staggerFromTo('.wide_4x .item', 0.5, {
-        y: -50,
-        opacity: 0
-    }, {
-        y: 0,
-        opacity: 1,
-    }, 0.1);
+TweenMax.staggerFromTo('.wide_4x .item', .8, {
+    //做事情
+    opacity: 0,
+    y: -50
+}, {
+    opacity: 1,
+    y: 0
+}, .3)
+
+TweenMax.staggerFromTo(['.box_1', '.box_2'], 2, {
+    //做事情
+    opacity: 0,
+    y: -50
+}, {
+    opacity: 1,
+    y: 0,
+    width: 400,
+    repeat: 1,
+    ease: Expo.easeOut
+},1)
+
+
+
+
+// var wp = new TimelineMax().staggerFromTo('.wide_4x .item', 0.5, {
+//         y: -50,
+//         opacity: 0
+//     }, {
+//         y: 0,
+//         opacity: 1,
+//     }, 0.1);
 
 
 
