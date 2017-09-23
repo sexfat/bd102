dedfb4718488ef3ae5145dc463147ab2c6440d36
sexfat <bryant6831@gmail.com>
2017-09-23 11:54:49 +0800

    add 滑鼠觸發事件

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -1,65 +1,100 @@
 // work
 
 $(function () {
+    //new 
+   
+    //
 
-TweenMax.fromTo(".section_1 .title", 1, {
-    y: -50,
-    opacity: 0,
-}, {
-    y: 0,
-    opacity: 1,
-    delay: 1,
-    ease: Bounce.easeOut
-});
+    TweenMax.fromTo(".section_1 .title", 1, {
+        y: -50,
+        opacity: 0,
+    }, {
+        y: 0,
+        opacity: 1,
+        delay: 1,
+        ease: Bounce.easeOut
+    });
 
-TweenMax.fromTo(".section_1 .content", 1, {
-    x: -50,
-    opacity: 0,
-}, {
-    x: 0,
-    opacity: 1,
-    delay: 1.5,
-    ease: Quad.easeOut
-});
-
-console.log('section_1 ok');
-
-//四個物件動畫
-TweenMax.staggerFromTo('.wide_4x .item', .8, {
-    //做事情
-    opacity: 0,
-    y: -50
-}, {
-    opacity: 1,
-    y: 0
-}, .3)
-
-TweenMax.staggerFromTo(['.box_1', '.box_2'], 2, {
-    //做事情
-    opacity: 0,
-    y: -50
-}, {
-    opacity: 1,
-    y: 0,
-    width: 400,
-    repeat: 1,
-    ease: Expo.easeOut
-},1)
+    TweenMax.fromTo(".section_1 .content", 1, {
+        x: -50,
+        opacity: 0,
+    }, {
+        x: 0,
+        opacity: 1,
+        delay: 1.5,
+        ease: Quad.easeOut
+    });
 
 
+    console.log('section_1 ok');
+
+    //四個物件動畫
 
 
-// var wp = new TimelineMax().staggerFromTo('.wide_4x .item', 0.5, {
-//         y: -50,
-//         opacity: 0
-//     }, {
-//         y: 0,
-//         opacity: 1,
-//     }, 0.1);
+ var controller = new ScrollMagic.Controller();
+
+    var tween_4 = TweenMax.staggerFromTo('.wide_4x .item', .8, {
+        //做事情
+        opacity: 0,
+        y: -50
+    }, {
+        opacity: 1,
+        y: 0
+    }, .3);
+
+
+    var scene_t = new ScrollMagic.Scene({
+      //做事情
+      triggerElement: "#trigger1",
+      duration: 450,
+      offset: 100
+    //   reverse: false,
+    })
+    .setTween(tween_4)
+    .addIndicators({
+            name: 'scene 01'
+        })
+   .addTo(controller);
+
+
+     
 
 
 
 
-console.log('section_2 ok');
+
+    TweenMax.staggerFromTo(['.box_1', '.box_2'], 2, {
+        //做事情
+        opacity: 0,
+        y: -50
+    }, {
+        opacity: 1,
+        y: 0,
+        width: 400,
+        repeat: 1,
+        repeatDelay: 2,
+        delay: .5,
+        ease: Expo.easeOut
+    }, 1)
+
+    // var tl =new TimelineMax();
+
+    // tl.staggerFromTo(['.box_1', '.box_2'], 2, {
+    //     //做事情
+    //     opacity: 0,
+    //     y: -50
+    // }, {
+    //     opacity: 1,
+    //     y: 0,
+    //     width: 400,
+    //     repeat: 1,
+    //     delay: .5,
+    //     ease: Expo.easeOut
+    // },1)
+
+    console.log('section_2 ok');
+
+
+
 
 })
